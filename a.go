8f45de827ed0102eafc84ae03e0e201b9a2c7a74
maki-engineer <ryohei-81@outlook.jp>
2024-11-03 11:22:03 +0900

    add: Some Sums success

--- a.go
+++ a.go
@@ -2,25 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-func ProblemA() {
-	var A, B, C, X int
+func mapFunc(strArray []string, f func(strNumber string) int) []int {
+	result := []int{}
+
+	for _, v := range strArray {
+		result = append(result, f(v))
+	}
+
+	return result
+}
+
+func sum(numberList []int) int {
 	result := 0
 
-	fmt.Scan(&A)
-	fmt.Scan(&B)
-	fmt.Scan(&C)
-	fmt.Scan(&X)
+	for _, v := range numberList {
+		result += v
+	}
 
-	for a := 0; a <= A; a++ {
-		for b := 0; b <= B; b++ {
-			for c := 0; c <= C; c++ {
-				totalAmount := (500 * a) + (100 * b) + (50 * c)
-				if totalAmount == X {
-					result++
-				}
-			}
+	return result
+}
+
+func ProblemA() {
+	var N, A, B int
+	result := 0
+
+	fmt.Scanf("%d %d %d", &N, &A, &B)
+
+	for i := 1; i <= N; i++ {
+		s := strconv.Itoa(i)
+
+		numberList := mapFunc(strings.Split(s, ""), func(strNumber string) int {
+			v, _ := strconv.Atoi(strNumber)
+
+			return v
+		})
+
+		sumTotal := sum(numberList)
+
+		if (A <= sumTotal) && (sumTotal <= B) {
+			result += i
 		}
 	}
 
